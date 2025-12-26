30c1e8df246e91646164bedfa15d9c7ba80291b6
Sekine <sekineiori0520@gmail.com>
2025-12-27 01:49:39 +0900

    lsp追加

---
+++ lua/plugins/lsp/cmp.lua
@@ -0,0 +1,60 @@
+-- nvim-cmpの設定ファイル
+return {
+  "hrsh7th/nvim-cmp",
+  event = "InsertEnter", -- 入力モードになったら読み込む
+  dependencies = {
+    "hrsh7th/cmp-nvim-lsp", -- LSPからの候補を出す
+    "hrsh7th/cmp-buffer",   -- 開いているファイル内の単語を出す
+    "hrsh7th/cmp-path",     -- ファイルパスを出す
+    "L3MON4D3/LuaSnip",     -- スニペットエンジン（必須）
+    "saadparwaiz1/cmp_luasnip",
+    "onsails/lspkind.nvim", -- 補完リストにアイコンを表示
+  },
+  config = function()
+    local cmp = require("cmp")
+    local lspkind = require("lspkind")
+
+    cmp.setup({
+      -- スニペットエンジンの設定
+      snippet = {
+        expand = function(args)
+          require("luasnip").lsp_expand(args.body)
+        end,
+      },
+      
+      -- 補完ウィンドウの見た目
+      window = {
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
+      },
+
+      -- アイコンを表示する設定
+      formatting = {
+        format = lspkind.cmp_format({
+          mode = 'symbol', 
+          maxwidth = 50,
+          ellipsis_char = '...',
+        }),
+      },
+
+      -- キー設定
+      mapping = cmp.mapping.preset.insert({
+        ["<C-k>"] = cmp.mapping.select_prev_item(), -- 前の候補へ (Ctrl+k)
+        ["<C-j>"] = cmp.mapping.select_next_item(), -- 次の候補へ (Ctrl+j)
+        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-f>"] = cmp.mapping.scroll_docs(4),
+        ["<C-Space>"] = cmp.mapping.complete(),     -- 手動で補完を出す
+        ["<C-e>"] = cmp.mapping.abort(),            -- 補完を閉じる
+        ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Enterで確定
+      }),
+
+      -- 補完のソース（優先順位順）
+      sources = cmp.config.sources({
+        { name = "nvim_lsp" }, -- LSP (変数や関数)
+        { name = "luasnip" },  -- スニペット
+        { name = "buffer" },   -- バッファ内の単語
+        { name = "path" },     -- ファイルパス
+      }),
+    })
+  end,
+}
---
+++ lua/plugins/lsp/lsp.lua
@@ -0,0 +1,50 @@
+--lsp
+return {
+  {
+    "williamboman/mason.nvim",
+    dependencies = {
+      "williamboman/mason-lspconfig.nvim",
+      "neovim/nvim-lspconfig",
+      "hrsh7th/cmp-nvim-lsp",
+    },
+    config = function()
+      -- 1. Mason（インストーラー）のセットアップ
+      require("mason").setup()
+
+      -- ★ここが最重要ポイント！
+      -- サーバーのリストをここで「変数」として作ります。
+      -- これがあるから、下のループでエラーになりません。
+      local servers = {
+        "lua_ls",
+        "pyright",
+        "ts_ls",
+        "html",
+        "cssls",
+        "bashls",
+      }
+
+      -- 2. Mason-LSPConfig のセットアップ
+      require("mason-lspconfig").setup({
+        -- ここでさっき作った変数(servers)を使います
+        ensure_installed = servers,
+        automatic_installation = true,
+      })
+
+      -- 3. 各サーバーの起動設定（手動ループ方式）
+      -- setup_handlers はバグの原因になるので使いません。
+      -- 代わりに、標準的な「for文」で回します。これなら確実に動きます。
+      
+      local lspconfig = require("lspconfig")
+      local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+      for _, server in ipairs(servers) do
+        -- もしサーバー設定が存在すればセットアップを実行
+        if lspconfig[server] then
+          lspconfig[server].setup({
+            capabilities = capabilities,
+          })
+        end
+      end
+    end,
+  }
+}
