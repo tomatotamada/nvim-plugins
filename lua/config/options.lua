08ff0edeaba8949012e73affbbe414546994cca5
Sekine <sekineiori0520@gmail.com>
2025-12-28 20:56:31 +0900

    縦横ライン追加

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,6 +1,6 @@
 -- 行番号を表示
 vim.opt.number = true
-vim.opt.relativenumber = false -- 相対行番号（ジャンプしやすくなる）
+vim.opt.relativenumber = true -- 相対行番号（ジャンプしやすくなる）
 
 -- クリップボードをOSと共有 (Ctrl+C / Ctrl+V と連携)
 vim.opt.clipboard = "unnamedplus"
@@ -13,10 +13,14 @@ vim.opt.autoindent = true
 
 -- 検索設定
 vim.opt.ignorecase = true -- 大文字小文字を区別しない
-vim.opt.smartcase = true  -- 大文字が入っているときだけ区別する
+vim.opt.smartcase = true -- 大文字が入っているときだけ区別する
 
 -- マウス有効化
 vim.opt.mouse = "a"
 
 -- 色の設定（フルカラー対応）
 vim.opt.termguicolors = true
+
+--縦横ラインの表示
+vim.opt.cursorline = true
+vim.opt.cursorcolumn = true
